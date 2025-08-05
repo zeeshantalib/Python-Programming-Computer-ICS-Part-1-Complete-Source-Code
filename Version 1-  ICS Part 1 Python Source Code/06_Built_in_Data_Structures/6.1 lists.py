f3c5d7ef4df255072beaca8a8a11c206af008ed7
Zeeshan Talib <zeeshantalib786@gmail.com>
2025-08-05 21:13:22 +0500

    Add files via upload

---
+++ Version 1-  ICS Part 1 Python Source Code/06_Built_in_Data_Structures/6.1 lists.py
@@ -0,0 +1,13 @@
+fruits = ["Mango", "Apple" "Banana"]
+print(fruits)
+
+# Output: [’Mango’ ’Apple ’ ’Banana ’ ]
+
+print(fruits[1])
+#Ouput: Apple
+
+fruits [0] = "Orange"
+fruits.append("Pineapple")
+print(fruits)
+# Output: [’Orange’, ’Apple’, ’Banana’, ’Pineapple’]
+
